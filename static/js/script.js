6cd6266ebd4a389aae262a218d536dc651701cdb
VPradeep <pradeepraja108@gmail.com>
2021-12-26 20:57:40 +0530

    personal portfolio website, first commit

---
+++ static/js/script.js
@@ -0,0 +1,68 @@
+$('.navToggle').on('click', function (e) {
+  e.preventDefault();
+  $('body').toggleClass('navToggleActive');
+});
+
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 10) {
+    $('body').addClass('fixedHeader');
+  } else {
+    $('body').removeClass('fixedHeader');
+  }
+});
+
+
+var swiper = new Swiper(".testimonialSwiper", {
+  navigation: {
+    nextEl: ".test-swiper-button-next",
+    prevEl: ".test-swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".certificatesSlider", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".cert-swiper-button-next",
+    prevEl: ".cert-swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+  },
+});
+
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+timeline
+  .to(".rock", 10, { y: -300 })
+  .to(".girl", 10, { y: -200 }, "-=10")
+  .fromTo(".bg1", { y: -50 }, { y: 0, duration: 10 }, "-=10")
+  .to(".content", 10, { top: "0%" }, "-=10")
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "300%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
